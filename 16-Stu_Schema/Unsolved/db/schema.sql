64ab8db7677290da1c1e204f3ac8b8b50a72fdea
KeyurMistry <keyurmist@gmail.com>
2022-06-23 20:53:23 +0100

    created tables for ex16

---
+++ 16-Stu_Schema/Unsolved/db/schema.sql
@@ -0,0 +1,18 @@
+-- Write your schema here --
+DROP DATABASE IF EXISTS product_db;
+CREATE DATABASE product_db;
+
+USE product_db;
+
+CREATE TABLE products (
+  id INT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  category_name VARCHAR(100) NOT NULL,
+  price INT NOT NULL,
+  in_stock BOOLEAN NOT NULL
+);
+
+CREATE TABLE categories (
+  id INT NOT NULL,
+  category_name VARCHAR(30) NOT NULL,
+);
